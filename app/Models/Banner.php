30ef4cb8ce2500604254c172f4d53ad8864a6356
Dang Khoa <tvdkhoa1801@gmail.com>
2023-07-26 16:33:04 +0700

    add banner

---
+++ app/Models/Banner.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Database\Eloquent\Model;
+
+class Banner extends Model
+{
+
+    protected $table = 'banner';
+    protected $guarded = [];
+}
